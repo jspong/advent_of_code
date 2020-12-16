10205019e6fbc5f506f52c6bd4c684e698cda983
John Spong <jsspong@amazon.com>
2020-12-16 03:48:31 -0600

    day 13 part 2; that stupid minus sign

--- 13/solution.py
+++ 13/solution.py
@@ -16,29 +16,39 @@ with open(sys.argv[1]) as f:
     _ = int(f.readline())
     schedule = process_input_line(f.readline())
 
-def gcd(x, y):
-    if y > x: return gcd(y,x)
-    elif y == 0:
-        return x
-    else:
-        return gcd(y, x % y)
+def inverse(a, b):
+    def _inverse(a, b):
+        if a == 0:
+            return b, 0, 1
 
-def are_coprime(numbers):
-    print(numbers)
-    numbers = [n for n in numbers if n is not None]
-    print(numbers)
-    for i, x in enumerate(numbers):
-        for y in numbers[i+1:]:
-            if gcd(x, y) != 1:
-                return (x, y)
-    return None
+        g, x1, y1 = _inverse(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1
+
+        return g, x, y
+    _, x, _ = _inverse(a, b)
+    return x % b
+
+def product(n):
+    value = 1
+    for x in n:
+        value *= x
+    return value
 
 def chinese_remainder(A, M):
-    return
+    x = 0
+    prod = product(M)
+    for a, m in zip(A, M):
+        b = prod // m
+        x += a * b * inverse(b, m)
+    x %= prod
+    return x
 
 def solution():
-    assert are_coprime(schedule) is None, are_coprime(schedule)
-    a, n = [i for i, x in enumerate(schedule) if x is not None], [x for x in schedule if x is not None]
-    return chinese_remainder(n, a)
+    a, m = [-i for i, x in enumerate(schedule) if x is not None], [x for x in schedule if x is not None]
+    answer = chinese_remainder(a, m)
+    for a, m in zip(a, m):
+        print(answer % m == a)
+    return answer
 
 print(solution())
