c8c5c4e2d5882f39f3317f41a8d52e1364c1723a
John Spong <jsspong@amazon.com>
2020-12-16 15:27:23 -0600

    2015 day 2 part 2

--- 2015/02/solution.py
+++ 2015/02/solution.py
@@ -4,13 +4,14 @@ import unittest
 class Test(unittest.TestCase):
 
     def tests(self):
-        self.assertEquals(solution('2x3x4'), 58)
-        self.assertEquals(solution('1x1x10'), 43)
+        self.assertEquals(solution('2x3x4'), 34)
+        self.assertEquals(solution('1x1x10'), 14)
 
 
 def solution(s):
-    parts = sorted(int(x) for x in s.split('x'))
-    return 3 * parts[0] * parts[1] + 2 * parts[1] * parts[2] + 2 * parts[2] * parts[0]
+    a, b, c = sorted(int(x) for x in s.split('x'))
+    perimeter = 2 * a + 2 * b
+    return perimeter + a * b * c
 
 if __name__ == '__main__':
     sum = 0
