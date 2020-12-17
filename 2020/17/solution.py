f67bc640b730253178d16cbd2733cce8a13e914e
John Spong <jsspong@amazon.com>
2020-12-17 17:13:49 -0600

    2020 day 17 part 2

--- 2020/17/solution.py
+++ 2020/17/solution.py
@@ -26,7 +26,7 @@ def process_line(state, i, line, handle):
     if state == 0:
         for x, c in enumerate(line):
             if c == '#':
-                part.append(((i, x, 0), 1))
+                part.append(((i, x, 0, 0), 1))
     elif state == 1:
         pass
     elif state == 2:
@@ -51,15 +51,18 @@ def main():
     x = 0, max(p[1] for p in positions) + 1
     y = 0, num_lines
     z = 0, 1
+    w = 0, 1
     for n in range(6):
         x = x[0] - 1, x[1] + 1
         y = y[0] - 1, y[1] + 1
         z = z[0] - 1, z[1] + 1
+        w = w[0] - 1, w[1] + 1
         new_positions = collections.defaultdict(int)
         for i in range(*x):
             for j in range(*y):
                 for k in range(*z):
-                    new_positions[(i,j,k)] = step(positions, i, j, k)
+                    for m in range(*w):
+                        new_positions[(i,j,k,m)] = step(positions, i, j, k, m)
         positions = new_positions
 
     return sum(positions.values())
@@ -72,20 +75,21 @@ def dump(positions, x, y, z):
                 print('#' if positions[(i, j, k)] else '.', end='')
             print()
 
-def step(positions, x, y, z):
-    if positions[(x,y,z)]:
-        return int(neighbors(positions, x, y, z) in (2, 3))
+def step(positions, x, y, z, w):
+    if positions[(x,y,z, w)]:
+        return int(neighbors(positions, x, y, z, w) in (2, 3))
     else:
-        return int(neighbors(positions, x, y, z) == 3)
+        return int(neighbors(positions, x, y, z, w) == 3)
 
-def neighbors(positions, x, y, z):
+def neighbors(positions, x, y, z, w):
     count = 0
     for i in (-1, 0, 1):
         for j in (-1, 0, 1):
             for k in (-1, 0, 1):
-                if i == 0 and j == 0 and k == 0:
-                    continue
-                count += positions[(x+i, y+j, z+k)]
+                for m in (-1, 0, 1):
+                    if i == 0 and j == 0 and k == 0 and m == 0:
+                        continue
+                    count += positions[(x+i, y+j, z+k, w+m)]
     return count
 
 
