184cab0ecb43c14fb506b6cacc600077d8bd6710
John Spong <jsspong@amazon.com>
2020-12-16 22:41:11 -0600

    update solution.py

--- 2020/solution.py
+++ 2020/solution.py
@@ -1,23 +1,57 @@
 #!/usr/local/bin/python3
+import itertools
+import re
 import sys
+import unittest
 
-parts = []
+def input_stream():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-':
+            return sys.stdin
+        else:
+            return open(sys.argv[1])
+    else:
+        return open('input.txt')
 
-def process_input_line(line):
-    return line
+states = list(range(10))
 
-if len(sys.argv) > 1:
-    input_ = sys.argv[1]
-else:
-    input_ = "input.txt"
+def process_line(state, line, handle, state_transition=lambda x, line: x):
+    original = line
+    part = None
+    if state == 0:
+        part = line
 
-with open(sys.argv[1]) as f:
-    for line in f:
-        line = line.strip()
-        part = process_input_line(line)
-        parts.append(part)
 
-def solution():
-    pass
+    elif state == 1:
+        pass
+    elif state == 2:
+        pass
+    elif state == 3:
+        pass
+    handle(part)
+    return state_transition(state, original)
 
-print(solution())
+def solution(line):
+    return
+
+def main():
+    state = 0
+    parts = []
+    with input_stream() as f:
+        for line in f:
+            line = line.strip()
+            state = process_line(state, line, parts.append)
+    count = 0
+    product = 1
+    entries = []
+    for part in parts:
+        pass
+    return
+
+class Tests(unittest.TestCase):
+
+    def test_self(self):
+        self.assertEquals(solution(''), None)
+
+if __name__ == '__main__':
+    print(main())
