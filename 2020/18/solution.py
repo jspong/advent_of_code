854333682231f562d06719a930406f230825d4f5
John Spong <jsspong@amazon.com>
2020-12-17 23:32:35 -0600

    2020 day 18 part 2

--- 2020/18/solution.py
+++ 2020/18/solution.py
@@ -30,7 +30,6 @@ def process_line(state, line, handle, state_transition=lambda x, line: x):
     return state_transition(state, original)
 
 def solution(line):
-    print(repr(line))
     while '(' in line:
         last = -1
         line = re.sub(r'\s*\(\s*', ' ( ', line)
@@ -44,25 +43,23 @@ def solution(line):
                 last = i
             elif c == ')':
                 sub = line[last+1:i-1].strip()
-                print(repr(sub))
                 line = line[:last] + '{}'.format(solution(sub)) + line[i+1:]
                 break
     current = None
     operator = None
-    for token in line.split():
-        if operator is not None:
-            value = int(token)
-            if operator == '+':
-                current += value
-            else:
-                current *= value
-            operator = None
-        else:
-            if current is None:
-                current = int(token)
-            else:
-                operator = token
-    return current
+    parts = line.split()
+
+    while '+' in parts:
+        plus = parts.index('+')
+        a, b = plus-1, plus+1
+        parts = parts[:a] + [int(parts[a]) + int(parts[b])] + parts[b+1:]
+
+    while '*' in parts:
+        plus = parts.index('*')
+        a, b = plus-1, plus+1
+        parts = parts[:a] + [int(parts[a]) * int(parts[b])] + parts[b+1:]
+
+    return parts[0]
 
 def main():
     state = 0
@@ -81,10 +78,11 @@ def main():
 class Tests(unittest.TestCase):
 
     def test_self(self):
-        self.assertEquals(solution('2 * 3 + (4 * 5)'), 26)
-        self.assertEquals(solution('5 + (8 * 3 + 9 + 3 * 4 * 3)'), 437)
-        self.assertEquals(solution('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'), 12240)
-        self.assertEquals(solution('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'), 13632)
+        self.assertEqual(solution('1 + (2 * 3) + (4 * (5 + 6))'), 51)
+        self.assertEqual(solution('2 * 3 + (4 * 5)'), 46)
+        self.assertEqual(solution('5 + (8 * 3 + 9 + 3 * 4 * 3)'), 1445)
+        self.assertEqual(solution('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'), 669060)
+        self.assertEqual(solution('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'), 23340)
 
 if __name__ == '__main__':
     print(main())
