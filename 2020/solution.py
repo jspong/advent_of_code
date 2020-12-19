18ee01238bb3008e4e7987a2f1c230dbf12e63ef
John Spong <jsspong@amazon.com>
2020-12-19 01:25:52 -0600

    solution.py fixups

--- 2020/solution.py
+++ 2020/solution.py
@@ -6,28 +6,17 @@ import operator
 import os
 import re
 import networkx
-import pygraphviz
-from networkx.drawing.nx_agraph import write_dot
 import sys
 import unittest
 
-def input_stream():
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '-':
-            return sys.stdin
-        else:
-            return open(sys.argv[1])
-    else:
-        return open('input.txt')
-
-states = list(range(10))
+import pygraphviz
+from networkx.drawing.nx_agraph import write_dot
 
 def process_line(state, line, handle, state_transition=lambda x, line: x):
     original = line
     part = None
     if state == 0:
         part = line
-
 
     elif state == 1:
         pass
@@ -39,10 +28,15 @@ def process_line(state, line, handle, state_transition=lambda x, line: x):
     return state_transition(state, original)
 
 def solution(line):
-    return
+    return 0
 
 def combine(results):
-    return sum(results)
+    return sum(solution(r) for r in results)
+
+class Tests(unittest.TestCase):
+
+    def test_self(self):
+        self.assertEquals(solution(''), None)
 
 def main():
     state = 0
@@ -64,16 +58,22 @@ def main():
     elif mapping:
         result = combine(mapping)
     elif g.edges():
+        write_dot(g, sys.stdout)
         result = combine(g)
     elif dg.edges():
+        write_dot(dg, sys.stdout)
         result = combine(dg)
 
     return result
 
-class Tests(unittest.TestCase):
-
-    def test_self(self):
-        self.assertEquals(solution(''), None)
+def input_stream():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-':
+            return sys.stdin
+        else:
+            return open(sys.argv[1])
+    else:
+        return open('input.txt')
 
 if __name__ == '__main__':
     print(main())
